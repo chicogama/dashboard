bd1292b7f85af4951adf8f6c05c214bce796460f
Josemar Gama <josemargama7@gmail.com>
2019-12-06 00:00:15 -0300

    Paginas

--- js/ajax.js
+++ js/ajax.js
@@ -1,15 +1,14 @@
 let content = document.getElementById('ajax-content')
-alert("ola")
-function fetchContent(el){
+function fetchContent(el) {
     let view = el.getAttribute('a-view')
-    let folder =  el.getAttribute('a-folder')
+    let folder = el.getAttribute('a-folder')
     fetch(`/ajax/${folder}/${view}.html`)
-    .then(response => {
-        let html = response.text()
-        return html
-    })
-    .then(html => {
-        console.log(html)
-        content.innerHTML = '<p>OLA MUNDO</p>'
-    })
+        .then(response => {
+            let html = response.text()
+            return html
+        })
+        .then(html => {
+            console.log(html)
+            content.innerHTML = html
+        })
 }
